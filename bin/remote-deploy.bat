2879570e609edc17be5fe129f1cfb7027ddab558
Peter Szanto <peter_szanto@epam.com>
2014-10-09 17:40:23 +0200

    adding remote deploy scripts

---
+++ bin/remote-deploy.bat
@@ -0,0 +1,1 @@
+c:\dev\tools\apache-storm-0.9.2-incubating\bin\storm jar c:\dev\src\storm-example\target\storm-example-1.0-SNAPSHOT.jar org.exampledriven.%1
---
+++ bin/remote-undeploy.bat
@@ -0,0 +1,1 @@
+c:\dev\tools\apache-storm-0.9.2-incubating\bin\storm kill %1
